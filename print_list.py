6adeae800d6aacce443173d42b8fc4b9f7d4fe7b
Luke Olali <luke.olali@andela.com>
2017-03-06 22:58:23 +0300

    write test for prime number

--- print_list.py
+++ print_list.py
@@ -1,10 +1,10 @@
-def prime_numbers(N):
+def prime_numbers(j,i,n,op):
 
-	for x in range(0,50):
+	for x in range(0,n):
 		if x<=0 or x==1:
 			for i in range(2,x):
 				pass
 		elif i%x==0 and x%x==0 and x%1==0:
 			print (x)
 
-	prime_numbers(7)		
+	prime_numbers()		
--- tdd.py
+++ tdd.py
@@ -5,7 +5,7 @@ class MyTest(unittest.TestCase):
 	def test.prime_numbers(self):
 		self.assertEqual(prime_numbers(3,3,"%"),0)
 		self.assertEqual(prime_numbers(3,1"%"),0)
-		self.assertEqual(prime_numbers(7%(5 in range 20),!0)
+		self.assertEqual(prime_numbers(7%(5 in range 50),!0)
 
 
        
