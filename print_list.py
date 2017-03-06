12ad1d1c0926c889d5d903c5b72b8a25918fb139
Luke Olali <luke.olali@andela.com>
2017-03-06 22:10:15 +0300

    This is my first commit

---
+++ print_list.py
@@ -0,0 +1,7 @@
+def prime_numbers(N):
+
+	for x in range(2,N):
+		if x<=0 or x==1:
+			pass
+		elif x%(x in range(2,N))!=0 and x%x==0 and x%1==0:
+			print (x)
---
+++ tdd.py
@@ -0,0 +1,11 @@
+import unittest
+import prime_numbers
+
+class MyTest(unittest.TestCase):
+	def test.prime_numbers(self):
+		self.assertTrue(prime_numbers(3,3,"%"),0)
+		self.assertTrue(prime_numbers(3,1"%"),0)
+		self.assertTrue(prime_numbers(7%(5 in range 20),!0)
+
+
+       
