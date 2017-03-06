cf711421c98072593cc65bde8b61fdd79e868ef6
Luke Olali <luke.olali@andela.com>
2017-03-06 22:42:43 +0300

    this is my first commit

--- print_list.py
+++ print_list.py
@@ -4,7 +4,7 @@ def prime_numbers(N):
 		if x<=0 or x==1:
 			for i in range(2,x):
 				pass
-		elif x%N==0 and x%x==0 and x%1==0:
+		elif i%x==0 and x%x==0 and x%1==0:
 			print (x)
 
 	prime_numbers(7)		
